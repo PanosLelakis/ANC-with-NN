8d57bef79f5b333a79e883e52243f8b0ff1bd302
Panos Lelakis <plelakis@gmail.com>
2025-07-27 09:51:03 +0200

    plot fixes

--- python/gui.py
+++ python/gui.py
@@ -2,13 +2,27 @@ import tkinter as tk
 from tkinter import ttk
 import threading
 import time
+import tkinter.font as tkfont
 from utils.audio_utils import play_audio, stop_audio
 from main import run_anc
 from utils.plot import *
 
+# Window size parameters
+aspect_ratio = 3 / 8
+initial_width = 200
+initial_height = int(initial_width / aspect_ratio)
+
 # Create the root window first
 root = tk.Tk()
 root.title("ANC with NN - Panos Lelakis")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = int(screen_width * 0.3)  # 30% of screen width
+window_height = int(screen_height * 0.8)  # 80% of screen height
+x_pos = int((screen_width - window_width) / 2)
+y_pos = int((screen_height - window_height) / 2)
+root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+root.minsize(initial_width, initial_height)
 
 # Global variables
 noisy_audio = None
@@ -28,11 +42,17 @@ stored_initial_weights = None
 stored_final_weights = None
 stored_signal_after_primary = None
 stored_signal_after_secondary = None
+all_buttons = []
+resizing = False # flag to avoid recursion
 
 def start_algorithm():
     global start_time, algorithm, mu, L, snr, noise_type
 
+    reset_result_labels()
     disable_buttons()
+    progress_var.set(0.0)
+    status_label.config(text="Running...", fg="black")
+
     start_time = time.time()
 
     # Get user input
@@ -48,23 +68,27 @@ def start_algorithm():
         snr = float(snr)
     except ValueError:
         status_label.config(text="Error: Invalid input!", fg="red")
-        enable_buttons()
+        start_button.config(state=tk.NORMAL)
         return
 
     # Run ANC in separate thread to avoid blocking GUI
     threading.Thread(target=run_anc, args=(algorithm, L, mu, snr, noise_type, update_progress, on_anc_complete), daemon=True).start()
 
 def disable_buttons():
-    # Disable buttons during processing
-    start_button.config(state=tk.DISABLED)
-    play_noisy_btn.config(state=tk.DISABLED)
-    play_filtered_btn.config(state=tk.DISABLED)
-    graph_button.config(state=tk.DISABLED)
+    global all_buttons
+    for b in all_buttons:
+        b.config(state=tk.DISABLED)
 
 def enable_buttons():
-    # Enable buttons after processing
-    start_button.config(state=tk.NORMAL)
-    graph_button.config(state=tk.NORMAL)
+    global all_buttons
+    for b in all_buttons:
+        b.config(state=tk.NORMAL)
+
+def reset_result_labels():
+    total_time_label.config(text="Total Time: --")
+    conv_time_label.config(text="Convergence Speed: --")
+    error_label.config(text="Steady-State Error: --")
+    status_label.config(text="Running...", fg="black")
 
 def update_progress(progress):
     # Update progress bar and estimated time
@@ -105,9 +129,8 @@ def on_anc_complete(reference_signal, noisy_signal, filtered_signal, error_signa
 
     root.after(0, lambda: status_label.config(text="Finished", fg="green"))
     root.after(0, enable_buttons)
-    root.after(0, lambda: play_noisy_btn.config(state=tk.NORMAL))
-    root.after(0, lambda: play_filtered_btn.config(state=tk.NORMAL))
-    root.after(0, lambda: graph_button.config(state=tk.NORMAL))
+    #if stored_signal_after_secondary == None:
+        #secondary_path_btn.config(state=tk.DISABLED)
 
     root.after(0, lambda: total_time_label.config(text=f"Total Time: {total_time:.2f} sec"))
     if convergence_speed is not None:
@@ -115,22 +138,6 @@ def on_anc_complete(reference_signal, noisy_signal, filtered_signal, error_signa
     else:
         root.after(0, lambda: conv_time_label.config(text="Convergence Speed: Not achieved"))
     root.after(0, lambda: error_label.config(text=f"Steady-State Error: {steady_state_error:.2f} dB"))
-
-def open_graphs():
-    global stored_reference_signal, noisy_audio, filtered_audio, stored_error_signal, stored_t, stored_fs
-    global algorithm, mu, L, noise_type, snr, convergence_speed, steady_state_error
-
-    plot_results(
-        stored_reference_signal, noisy_audio, filtered_audio, stored_error_signal,
-        stored_t, stored_fs,
-        algorithm_name=algorithm,
-        mu=mu,
-        L=L,
-        noise_type=noise_type,
-        snr=snr,
-        convergence_time=convergence_speed,
-        steady_state_error=steady_state_error
-    )
 
 def play_noisy_signal():
     global is_playing
@@ -145,15 +152,7 @@ def play_noisy_signal():
     play_noisy_btn.config(text="Stop playing", state=tk.NORMAL)
     play_filtered_btn.config(state=tk.DISABLED)
 
-    def play():
-        try:
-            play_audio(noisy_audio)
-        except Exception as e:
-            print(f"Error playing noisy signal: {e}")
-        finally:
-            root.after(0, reset_play_buttons)
-
-    threading.Thread(target=play, daemon=True).start()
+    threading.Thread(target=play_audio_thread, args=[noisy_audio], daemon=True).start()
 
 def play_filtered_signal():
     global is_playing
@@ -168,15 +167,15 @@ def play_filtered_signal():
     play_filtered_btn.config(text="Stop playing", state=tk.NORMAL)
     play_noisy_btn.config(state=tk.DISABLED)
 
-    def play():
+    threading.Thread(target=play_audio_thread, args=[filtered_audio], daemon=True).start()
+
+def play_audio_thread(audio):
         try:
-            play_audio(filtered_audio)
+            play_audio(audio)
         except Exception as e:
-            print(f"Error playing filtered signal: {e}")
+            print(f"Error playing signal: {e}")
         finally:
             root.after(0, reset_play_buttons)
-
-    threading.Thread(target=play, daemon=True).start()
 
 def reset_play_buttons():
     global is_playing
@@ -272,67 +271,153 @@ def plot_signal():
         steady_state_error=steady_state_error
     )
 
+# Create a custom font style
+font_size = max(11, int(screen_height / 70))  # Minimum size 10, scales with screen
+default_font = tkfont.nametofont("TkDefaultFont")
+default_font.configure(size=font_size)  # Increase default font size
+root.option_add("*Font", default_font)
+
 # GUI Elements
-tk.Label(root, text="Select Algorithm:").grid(row=0, column=0)
+tk.Label(root, text="Select Algorithm:", font=default_font).grid(row=0, column=0)
 algo_var = tk.StringVar(value="LMS")
-algo_menu = ttk.Combobox(root, textvariable=algo_var, values=["LMS", "NLMS", "FxLMS", "FxNLMS"])
+algo_menu = ttk.Combobox(root, textvariable=algo_var, values=["LMS", "NLMS", "FxLMS", "FxNLMS"], font=default_font)
 algo_menu.grid(row=0, column=1)
 
-tk.Label(root, text="Mu:").grid(row=1, column=0)
-mu_entry = tk.Entry(root)
+tk.Label(root, text="Mu:", font=default_font).grid(row=1, column=0)
+mu_entry = tk.Entry(root, font=default_font)
 mu_entry.grid(row=1, column=1)
 
-tk.Label(root, text="L:").grid(row=2, column=0)
-L_entry = tk.Entry(root)
+tk.Label(root, text="L:", font=default_font).grid(row=2, column=0)
+L_entry = tk.Entry(root, font=default_font)
 L_entry.grid(row=2, column=1)
 
-tk.Label(root, text="Select Noise Type:").grid(row=3, column=0)
+tk.Label(root, text="Select Noise Type:", font=default_font).grid(row=3, column=0)
 noise_var = tk.StringVar(value="White")
-noise_menu = ttk.Combobox(root, textvariable=noise_var, values=["White", "Pink", "Brownian", "Violet", "Grey", "Blue"])
+noise_menu = ttk.Combobox(root, textvariable=noise_var, values=["White", "Pink", "Brownian", "Violet", "Grey", "Blue"], font=default_font)
 noise_menu.grid(row=3, column=1)
 
-tk.Label(root, text="SNR (dB):").grid(row=4, column=0)
-snr_entry = tk.Entry(root)
+tk.Label(root, text="SNR (dB):", font=default_font).grid(row=4, column=0)
+snr_entry = tk.Entry(root, font=default_font)
 snr_entry.grid(row=4, column=1)
 
-start_button = tk.Button(root, text="Start", command=start_algorithm)
+start_button = tk.Button(root, text="Start", font=default_font, command=start_algorithm)
 start_button.grid(row=5, column=0, columnspan=2)
+all_buttons.append(start_button)
 
-play_noisy_btn = tk.Button(root, text="Play Noisy Signal", state=tk.DISABLED, command=play_noisy_signal)
+play_noisy_btn = tk.Button(root, text="Play Noisy Signal", state=tk.DISABLED, font=default_font, command=play_noisy_signal)
 play_noisy_btn.grid(row=6, column=0)
+all_buttons.append(play_noisy_btn)
 
-play_filtered_btn = tk.Button(root, text="Play Filtered Signal", state=tk.DISABLED, command=play_filtered_signal)
+play_filtered_btn = tk.Button(root, text="Play Filtered Signal", state=tk.DISABLED, font=default_font, command=play_filtered_signal)
 play_filtered_btn.grid(row=6, column=1)
+all_buttons.append(play_filtered_btn)
 
-graph_button = tk.Button(root, text="Graphs", state=tk.DISABLED, command=open_graphs)
-graph_button.grid(row=7, column=0, columnspan=2)
+status_label = tk.Label(root, text="", fg="black", font=default_font)
+status_label.grid(row=7, column=0, columnspan=2)
 
-status_label = tk.Label(root, text="", fg="black")
-status_label.grid(row=8, column=0, columnspan=2)
-
-time_label = tk.Label(root, text="Time remaining: 00:00", fg="black")
-time_label.grid(row=9, column=0, columnspan=2)
+time_label = tk.Label(root, text="Time remaining: 00:00", fg="black", font=default_font)
+time_label.grid(row=8, column=0, columnspan=2)
 
 progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
-progress_bar.grid(row=10, column=0, columnspan=2, pady=10)
+progress_bar.grid(row=9, column=0, columnspan=2, pady=10)
 
-total_time_label = tk.Label(root, text="Total Time: --")
-total_time_label.grid(row=11, column=0, columnspan=2)
+total_time_label = tk.Label(root, text="Total Time: --", font=default_font)
+total_time_label.grid(row=10, column=0, columnspan=2)
 
-conv_time_label = tk.Label(root, text="Convergence Speed: --")
-conv_time_label.grid(row=12, column=0, columnspan=2)
+conv_time_label = tk.Label(root, text="Convergence Speed: --", font=default_font)
+conv_time_label.grid(row=11, column=0, columnspan=2)
 
-error_label = tk.Label(root, text="Steady-State Error: --")
-error_label.grid(row=13, column=0, columnspan=2)
+error_label = tk.Label(root, text="Steady-State Error: --", font=default_font)
+error_label.grid(row=12, column=0, columnspan=2)
 
 # --- Diagnostic Graphs Frame ---
-diagnostics_frame = tk.LabelFrame(root, text="Graphs")
-diagnostics_frame.grid(row=14, column=0, columnspan=2, pady=10)
+diagnostics_frame = tk.LabelFrame(root, text="Graphs", font=default_font)
+diagnostics_frame.grid(row=13, column=0, columnspan=2, pady=10)
 
-tk.Button(diagnostics_frame, text="Filter Weights", command=plot_filter).grid(row=0, column=0, sticky="ew", padx=5, pady=2)
-tk.Button(diagnostics_frame, text="Primary Path", command=plot_primary_path_effect).grid(row=1, column=0, sticky="ew", padx=5, pady=2)
-tk.Button(diagnostics_frame, text="Secondary Path", command=plot_secondary_path_effect).grid(row=2, column=0, sticky="ew", padx=5, pady=2)
-tk.Button(diagnostics_frame, text="Error Analysis", command=plot_error).grid(row=3, column=0, sticky="ew", padx=5, pady=2)
-tk.Button(diagnostics_frame, text="Signal Flow", command=plot_signal).grid(row=4, column=0, sticky="ew", padx=5, pady=2)
+filter_weights_btn = tk.Button(diagnostics_frame, text="Filter Weights", state=tk.DISABLED, font=default_font, command=plot_filter)
+filter_weights_btn.grid(row=0, column=0, sticky="ew", padx=5, pady=2)
+all_buttons.append(filter_weights_btn)
+
+primary_path_btn = tk.Button(diagnostics_frame, text="Primary Path", state=tk.DISABLED, font=default_font, command=plot_primary_path_effect)
+primary_path_btn.grid(row=1, column=0, sticky="ew", padx=5, pady=2)
+all_buttons.append(primary_path_btn)
+
+secondary_path_btn = tk.Button(diagnostics_frame, text="Secondary Path", state=tk.DISABLED, font=default_font, command=plot_secondary_path_effect)
+secondary_path_btn.grid(row=2, column=0, sticky="ew", padx=5, pady=2)
+all_buttons.append(secondary_path_btn)
+
+error_analysis_btn = tk.Button(diagnostics_frame, text="Error Analysis", state=tk.DISABLED, font=default_font, command=plot_error)
+error_analysis_btn.grid(row=3, column=0, sticky="ew", padx=5, pady=2)
+all_buttons.append(error_analysis_btn)
+
+signal_flow_btn = tk.Button(diagnostics_frame, text="Signal Flow", state=tk.DISABLED, font=default_font, command=plot_signal)
+signal_flow_btn.grid(row=4, column=0, sticky="ew", padx=5, pady=2)
+all_buttons.append(signal_flow_btn)
+
+def on_resizee(event):
+    # Lock aspect ratio
+    desired_height = int(event.width / aspect_ratio)
+    if event.height != desired_height:
+        root.geometry(f"{event.width}x{desired_height}")
+        return  # Skip font scaling until next event
+
+    # Adjust font size based on height
+    scale = event.height / initial_height
+    new_font_size = int(10 * scale)
+
+    new_font = tkfont.Font(size=new_font_size)
+
+    # Apply font to all children
+    def apply_font_recursive(widget):
+        try:
+            widget.configure(font=new_font)
+        except:
+            pass
+        for child in widget.winfo_children():
+            apply_font_recursive(child)
+
+    apply_font_recursive(root)
+
+def on_resize(event):
+    global resizing
+    if resizing:
+        return  # prevent recursion
+
+    resizing = True
+
+    try:
+        # Lock aspect ratio
+        desired_width = event.width
+        desired_height = int(desired_width / aspect_ratio)
+
+        # Prevent infinite loop by checking if size is already close
+        if abs(event.height - desired_height) > 2:
+            root.geometry(f"{desired_width}x{desired_height}")
+
+        # Dynamic font resizing (proportional to height)
+        font_size = max(8, int(desired_height / 40))  # tune divisor as needed
+        default_font.configure(size=font_size)
+
+    finally:
+        resizing = False
+
+# Add padding to all widgets
+#for child in root.winfo_children():
+    #child.grid_configure(padx=5, pady=5)
+    
+#for child in diagnostics_frame.winfo_children():
+    #child.grid_configure(padx=5, pady=5)
+
+# Make the progress bar thicker
+progress_bar.config(length=window_width-10)  # Subtract some pixels for padding
+
+for i in range(root.grid_size()[0]):
+    root.columnconfigure(i, weight=1)
+
+for i in range(root.grid_size()[1]):
+    root.rowconfigure(i, weight=1)
+
+#root.resizable(True, True)
+#root.bind("<Configure>", on_resize)
 
 root.mainloop()
--- python/utils/plot.py
+++ python/utils/plot.py
@@ -4,124 +4,6 @@ from utils.smoothing import smooth_signal
 from utils.convert_to_dbfs import convert_to_dbfs
 from utils.fft_transform import compute_fft
 from matplotlib.ticker import ScalarFormatter
-from matplotlib.widgets import CheckButtons
-
-def plot_results(reference_signal, noisy_signal, filtered_signal, error_signal, t, fs,
-                 algorithm_name="", mu=None, L=None, noise_type="", snr=None,
-                 convergence_time=None, steady_state_error=None):
-
-    # Convert signals to dBFS
-    #max_val = np.max(np.abs(reference_signal))
-    #reference_signal_dbfs = convert_to_dbfs(reference_signal, max_val)
-    #noisy_signal_dbfs = convert_to_dbfs(noisy_signal, max_val)
-    #filtered_signal_dbfs = convert_to_dbfs(filtered_signal, max_val)
-    #error_signal_dbfs = convert_to_dbfs(error_signal, max_val)
-
-    # Smoothed signals for visualization
-    #reference_signal_dbfs_median = smooth_signal(reference_signal_dbfs, 401)
-    #noisy_signal_dbfs_median = smooth_signal(noisy_signal_dbfs, 401)
-    #filtered_signal_dbfs_median = smooth_signal(filtered_signal_dbfs, 401)
-    #error_signal_dbfs_median = smooth_signal(error_signal_dbfs, 401)
-    error_signal_db_median = smooth_signal(20 * np.log10(abs(error_signal + 1e-10)), 401)
-
-    # Compute FFT of the error signal
-    freqs, error_signal_fft = compute_fft(error_signal, fs)
-
-    # Plot results
-    plt.figure()
-
-    # Create the simulation metadata title
-    title_line1 = f"Algorithm: {algorithm_name} | μ: {mu} | L: {L} | Noise: {noise_type} | SNR: {snr} dB"
-
-    # Format the convergence time
-    if convergence_time is None:
-        convergence_str = "Convergence Speed: N/A"
-    else:
-        convergence_str = f"Convergence Speed: {convergence_time:.2f} sec"
-
-    # Format steady-state error
-    if steady_state_error is None:
-        error_str = "Steady-State Error: N/A"
-    else:
-        error_str = f"Steady-State Error: {steady_state_error:.2f} dB"
-
-    title_line2 = f"{convergence_str} | {error_str}"
-
-    # Combine both lines into the suptitle
-    plt.suptitle(f"{title_line1}\n{title_line2}", fontsize=11, fontweight='bold')
-
-    # Plot signals in time domain
-    plt.subplot(2, 2, 1)
-    #plt.plot(t, reference_signal, label="Original Signal", alpha=0.5)
-    #plt.plot(t, noisy_signal, label="Noisy Signal", alpha=0.5)
-    #plt.plot(t, filtered_signal, label="Filtered Signal", alpha=0.5)
-    plt.plot(t, error_signal, label="Error Signal")
-    plt.legend()
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude")
-    plt.gca().yaxis.set_major_formatter(ScalarFormatter())
-    #plt.gca().set_yticks([-3, -2, -1, 0, 1, 2, 3])
-    plt.gca().minorticks_on()
-    #plt.ylim([-3, 3])  # Set y-axis limit
-    plt.xlim([0, 2])
-    plt.grid()
-
-    # Plot signals in dBFS
-    plt.subplot(2, 2, 2)
-    #plt.plot(t, reference_signal_dbfs, label="Original Signal", alpha=0.5)
-    #plt.plot(t, noisy_signal_dbfs, label="Noisy Signal", alpha=0.5)
-    #plt.plot(t, filtered_signal_dbfs, label="Filtered Signal", alpha=0.5)
-    #plt.plot(t, error_signal_dbfs, label="Error Signal", alpha=0.5)
-    plt.plot(t, 20 * np.log10(abs(error_signal + 1e-10)), label="Error Signal")
-    plt.legend()
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude (dB)")
-    plt.gca().yaxis.set_major_formatter(ScalarFormatter())
-    #plt.gca().set_yticks([-60, -40, -20, 0, 20])
-    plt.gca().minorticks_on()
-    #plt.ylim([-60, 20])  # Set y-axis limit
-    plt.xlim([0, 2])
-    plt.grid()
-
-    # Plot in frequency domain
-    plt.subplot(2, 2, 3)
-    plt.plot(freqs, error_signal_fft, label="Error Signal FFT")
-    #plt.plot(t, reference_signal_dbfs_median, label="Original Signal", alpha=0.5)
-    #plt.plot(t, noisy_signal_dbfs_median, label="Noisy Signal", alpha=0.5)
-    #plt.plot(t, filtered_signal_dbfs_median, label="Filtered Signal", alpha=0.5)
-    #plt.plot(t, error_signal_dbfs_median, label="Error Signal", alpha=0.5)
-    plt.legend()
-    plt.xlabel("Frequency (kHz)")
-    plt.ylabel("Amplitude (dB)")
-    plt.gca().set_xscale('log')
-    plt.gca().xaxis.set_major_formatter(ScalarFormatter())
-    #plt.gca().set_xticks([20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
-    plt.gca().yaxis.set_major_formatter(ScalarFormatter())
-    #plt.gca().set_yticks([-60, -40, -20, 0, 20])
-    plt.gca().minorticks_on()
-    plt.xlim([20, 10000])
-    #plt.ylim([-20, 60])
-    plt.grid()
-
-    # Plot smoothed signals
-    plt.subplot(2, 2, 4)
-    #plt.plot(t, reference_signal_dbfs_median, label="Original Signal", alpha=0.5)
-    #plt.plot(t, noisy_signal_dbfs_median, label="Noisy Signal", alpha=0.5)
-    #plt.plot(t, filtered_signal_dbfs_median, label="Filtered Signal", alpha=0.5)
-    #plt.plot(t, error_signal_dbfs_median, label="Error Signal", alpha=0.5)
-    plt.plot(t, error_signal_db_median, label="Error Signal")
-    plt.legend()
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude (dBFS) - Smoothed")
-    plt.gca().yaxis.set_major_formatter(ScalarFormatter())
-    #plt.gca().set_yticks([-3, -2, -1, 0, 1, 2, 3])
-    plt.gca().minorticks_on()
-    #plt.ylim([-3, 3])  # Set y-axis limit
-    plt.xlim([0, 2])
-    plt.grid()
-
-    plt.tight_layout()
-    plt.show()
 
 def figure_title_metadata(algorithm_name="", mu=None, L=None, noise_type="", snr=None,
                  convergence_time=None, steady_state_error=None, title_line3=""):
@@ -147,24 +29,6 @@ def figure_title_metadata(algorithm_name="", mu=None, L=None, noise_type="", snr
         # Combine both lines into the suptitle
         plt.suptitle(f"{title_line1}\n{title_line2}\n{title_line3}", fontsize=11, fontweight='bold')
 
-def add_toggle_panel(fig, raw_lines, smooth_lines):
-    # Add checkbox axes to the right of the plot (x, y, width, height)
-    toggle_ax = fig.add_axes([0.85, 0.4, 0.13, 0.2])
-    labels = [f"Line {i+1}" for i in range(len(raw_lines))]
-
-    # Initial checkbox states (True = show raw)
-    visibility = [True] * len(raw_lines)
-
-    check = CheckButtons(toggle_ax, labels, visibility)
-
-    def toggle(label):
-        index = labels.index(label)
-        raw_lines[index].set_visible(not raw_lines[index].get_visible())
-        smooth_lines[index].set_visible(not smooth_lines[index].get_visible())
-        plt.draw()
-
-    check.on_clicked(toggle)
-
 def plot_filter_weights(fs, w_final, #w_initial, 
                  algorithm_name="", mu=None, L=None, noise_type="", snr=None,
                  convergence_time=None, steady_state_error=None):
@@ -173,10 +37,7 @@ def plot_filter_weights(fs, w_final, #w_initial,
     
     plt.figure()
     figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
-                 convergence_time, steady_state_error, "Filter Weights (Initial vs Final)")
-
-    plt.subplot(2, 1, 1)
-    #plt.plot(w_initial, label="Initial")
+                 convergence_time, steady_state_error, "Filter Weights")
     plt.plot(w_final, label="Final Weights")
     plt.title("Filter Weights (Time Domain)")
     plt.xlabel("Coefficient Index")
@@ -185,7 +46,9 @@ def plot_filter_weights(fs, w_final, #w_initial,
     plt.grid()
     plt.legend()
 
-    plt.subplot(2, 1, 2)
+    plt.figure()
+    figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
+                 convergence_time, steady_state_error, "Filter Weights")
     plt.plot(freqs, w_fft, label="FFT of Weights")
     plt.title("Filter Weights (Frequency Domain)")
     plt.xlabel("Frequency (Hz)")
@@ -210,8 +73,6 @@ def plot_path_analysis(path_ir, signal_before, signal_after, t, fs, title_prefix
     figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
                           convergence_time, steady_state_error,
                           f"{title_prefix} Path Frequency Domain")
-
-    plt.subplot(2, 1, 1)
     plt.plot(freqs_path, path_fft, label="Impulse Response FFT")
     plt.title(f"{title_prefix} Path Frequency Response")
     plt.xlabel("Frequency (Hz)")
@@ -221,7 +82,10 @@ def plot_path_analysis(path_ir, signal_before, signal_after, t, fs, title_prefix
     plt.grid()
     plt.legend()
 
-    plt.subplot(2, 1, 2)
+    plt.figure()
+    figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
+                          convergence_time, steady_state_error,
+                          f"{title_prefix} Path Frequency Domain")
     plt.plot(freqs_before, before_fft, label="Input Signal", alpha=0.7)
     plt.plot(freqs_after, after_fft, label="After Convolution", alpha=0.7)
     plt.title(f"Signal Before & After {title_prefix} Path (FFT)")
@@ -238,10 +102,6 @@ def plot_path_analysis(path_ir, signal_before, signal_after, t, fs, title_prefix
 def plot_error_analysis(error_signal, t, fs, passive_cancelling=None,
                  algorithm_name="", mu=None, L=None, noise_type="", snr=None,
                  convergence_time=None, steady_state_error=None):
-    
-    #error_smooth = smooth_signal(error_signal, 401)
-    #error_db = 20 * np.log10(np.abs(error_signal + 1e-10))
-    #error_db_smooth = smooth_signal(error_db, 401)
 
     # Use last 20% of samples
     start_idx = int(0.8 * len(error_signal))
@@ -261,8 +121,6 @@ def plot_error_analysis(error_signal, t, fs, passive_cancelling=None,
     plt.figure()
     figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
                  convergence_time, steady_state_error, "Error Signal Analysis")
-
-    plt.subplot(2, 1, 1)
     if passive_cancelling is not None:
         plt.plot(t, passive_dbfs_smooth, label="Passive Cancelling", linestyle="--", alpha=0.7)
         plt.plot(t, error_dbfs_smooth, label="Active Cancelling", linestyle="-", alpha=0.7)
@@ -275,7 +133,9 @@ def plot_error_analysis(error_signal, t, fs, passive_cancelling=None,
     plt.legend()
     plt.grid()
 
-    plt.subplot(2, 1, 2)
+    plt.figure()
+    figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
+                 convergence_time, steady_state_error, "Error Signal Analysis")
     if passive_cancelling is not None:
         plt.plot(freqs, passive_fft, label="Passive Cancelling FFT", alpha=0.7)
         plt.plot(freqs, error_fft, label="Active Cancelling FFT", alpha=0.7)
@@ -291,10 +151,6 @@ def plot_error_analysis(error_signal, t, fs, passive_cancelling=None,
     plt.legend()
     plt.grid()
 
-    # Add toggle after plotting
-    #add_toggle_panel(plt.gcf(), [raw1, raw2], [smooth1, smooth2])
-
-    #plt.tight_layout(rect=[0, 0, 0.85, 1])  # Leave space for the toggle panel
     plt.tight_layout()
     plt.show()
 
@@ -302,26 +158,51 @@ def plot_signal_flow(reference, noisy, filtered, t,
                  algorithm_name="", mu=None, L=None, noise_type="", snr=None,
                  convergence_time=None, steady_state_error=None):
     
+    max_value = np.max(abs(noisy))
+    reference_dbfs = convert_to_dbfs(reference, max_value)
+    noisy_dbfs = convert_to_dbfs(noisy, max_value)
+    filtered_dbfs = convert_to_dbfs(filtered, max_value)
+
     plt.figure()
     figure_title_metadata(algorithm_name, mu, L, noise_type, snr,
                  convergence_time, steady_state_error, "Signal Flow Comparison")
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(2, 2, 1)
     plt.plot(t, reference, label="Reference", alpha=0.7)
     plt.plot(t, noisy, label="Noisy", alpha=0.7)
-    plt.title("Reference vs Noisy")
+    plt.title("Reference vs Noisy (Time Domain)")
     plt.xlabel("Time (sec)")
     plt.ylabel("Amplitude")
     plt.xlim([0, 1])
     plt.legend()
     plt.grid()
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(2, 2, 2)
     plt.plot(t, noisy, label="Noisy", alpha=0.7)
     plt.plot(t, filtered, label="Filtered", alpha=0.7)
-    plt.title("Noisy vs Filtered")
+    plt.title("Noisy vs Filtered (Time Domain)")
     plt.xlabel("Time (sec)")
     plt.ylabel("Amplitude")
+    plt.xlim([0, 1])
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(2, 2, 3)
+    plt.plot(t, reference_dbfs, label="Reference", alpha=0.7)
+    plt.plot(t, noisy_dbfs, label="Noisy", alpha=0.7)
+    plt.title("Reference vs Noisy (Time Domain)")
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Amplitude (dBFS)")
+    plt.xlim([0, 1])
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(2, 2, 4)
+    plt.plot(t, noisy_dbfs, label="Noisy", alpha=0.7)
+    plt.plot(t, filtered_dbfs, label="Filtered", alpha=0.7)
+    plt.title("Noisy vs Filtered (Time Domain)")
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Amplitude (dBFS)")
     plt.xlim([0, 1])
     plt.legend()
     plt.grid()
